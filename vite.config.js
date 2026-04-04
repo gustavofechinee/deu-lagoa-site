ce0ef6a561a7761445d464a6846a9e68efa7d63b
Gustavo Fechine <89873229+gustavofechinee@users.noreply.github.com>
2026-04-04 20:17:01 -0300

    Revert "Rebuild Deu Lagoa site with React and Tailwind"
    This reverts commit 45b7b2972cfc9de44536a4bc4fe2047b2b979a7f.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: './',
-  plugins: [react(), tailwindcss()],
+  base: "./",
 });
